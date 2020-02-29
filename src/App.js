4fa10dae99960f70a32e6a81dccf8ea07338486d
paolo gonzales <paologonzales1616@gmail.com>
2020-02-29 18:25:57 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import QrReader from "react-qr-reader";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={12} md={{ span: 6, offset: 3 }}>
+          <QrReader
+            delay={300}
+            onError={err => console.log(err)}
+            onScan={data => console.log(data)}
+            style={{ width: "100%" }}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
